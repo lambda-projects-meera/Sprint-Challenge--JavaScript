c9c6915e0d01f1bcc3e8ae8214c7f0965bb3c912
meera-andersen <meera.andersen@gmail.com>
2019-08-22 14:25:48 -0600

    completed first 3 array challenges

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -72,11 +72,13 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 {"id":9,"first_name":"Michail","university":"Universidad Católica de Ávila","email":"mrome8@shinystat.com"},
 {"id":10,"first_name":"Hube","university":"Universitat Rovira I Virgili Tarragona","email":"hlethbrig9@foxnews.com"}]
 
-/* Request 1: Create a new array called universities that contains all the universities in the graduates array.  
+/* Request 1: Create a new array called universities that contains all the universities in the graduates array. 
+    - Once you have the new array created, sort the universities alphabetically and log the result. */
+const universities = graduates.map(function(grad){
+  return grad.university;
+})
+console.log(universities.sort());
 
-Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = [];
-console.log(universities)
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
@@ -84,13 +86,31 @@ The resulting contact information should have a space between the first name and
 Name email@example.com
 
 Log the result of your new array. */
-const contactInfo = [];
+
+
+const contactInfo = graduates.map(function(grad){
+  return `${grad.first_name} ${grad.email}`
+});
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
+// const uni = graduates.filter(function(grad){
+//   if (grad.university.includes("Uni")){
+//     return `${grad.university}`;
+//   }
+// });
+// console.log(uni);
+
 const uni = [];
-console.log(uni);
+
+for (let i = 0; i < graduates.length; i++) {
+  if (graduates[i].university.includes("Uni")){
+    uni.push(graduates[i].university);
+  }
+}
+
+console.log(uni)
 
 
 // ==== ADVANCED Array Methods ====
